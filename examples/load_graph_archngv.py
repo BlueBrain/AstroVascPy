e6a22882b50bc679e9d4b2f7b294284fcdaf148e
Christos Kotsalos <christos.kotsalos@epfl.ch>
2023-08-16 08:56:53 +0200

    Add two parallelization backends: Either multiprocessing or joblib

--- examples/load_graph_archngv.py
+++ examples/load_graph_archngv.py
@@ -1,3 +1,4 @@
+import multiprocessing
 from joblib import Parallel, delayed, parallel_config
 import pickle
 from functools import partial
@@ -16,13 +17,14 @@ import psutil
 
 print = partial(print, flush=True)
 
-def load_graph_archngv_parallel(filename, n_workers, n_astro=None):
+def load_graph_archngv_parallel(filename, n_workers, n_astro=None, parallelization_backend="multiprocessing"):
     """Load a vasculature from an NGV circuit.
 
     Args:
         filename (str): vasculature dataset.
         n_workers (int): number of processes to set endfeet on edges.
         n_astro (int): for testing, if not None, it will reduce the number of astrocytes used
+        parallelization_backend (str): Either multiprocessing or joblib
 
     Returns:
         vasculatureAPI.PointVasculature: graph containing point vasculature skeleton.
@@ -56,18 +58,33 @@ def load_graph_archngv_parallel(filename, n_workers, n_astro=None):
         for endfoot_id in gv_conn.astrocyte_endfeet(astro_id)
     ]
 
-    with parallel_config(backend="loky", prefer="processes", n_jobs=n_workers, inner_max_num_threads=1):
-        parallel = Parallel(return_as="generator", batch_size="auto")
-        parallelized_region = parallel(delayed(worker)(arg) for arg in tqdm(args, total=len(endfoot_ids)))
-        
-        for result_ids, result_endfeet in zip(
-            parallelized_region,
-            endfoot_ids
-        ):
-            # Only the main process executes this part, i.e. as soon as it receives the parallelly generated data
-            graph.edge_properties.loc[
-                pd.MultiIndex.from_arrays(result_ids.T), "endfeet_id"
-            ] = result_endfeet
+    if parallelization_backend == "multiprocessing":
+        with multiprocessing.Pool(n_workers) as pool:
+            for result_ids, result_endfeet in zip(
+                tqdm(
+                    pool.imap(worker, args, chunksize=max(1, int(len(endfoot_ids) / n_workers))),
+                    total=len(endfoot_ids),
+                ),
+                endfoot_ids,
+            ):
+                # Only the main process executes this part, i.e. as soon as it receives the parallelly generated data
+                graph.edge_properties.loc[
+                    pd.MultiIndex.from_arrays(result_ids.T), "endfeet_id"
+                ] = result_endfeet
+
+    if parallelization_backend == "joblib":
+        with parallel_config(backend="loky", prefer="processes", n_jobs=n_workers, inner_max_num_threads=1):
+            parallel = Parallel(return_as="generator", batch_size="auto")
+            parallelized_region = parallel(delayed(worker)(arg) for arg in tqdm(args, total=len(endfoot_ids)))
+            
+            for result_ids, result_endfeet in zip(
+                parallelized_region,
+                endfoot_ids
+            ):
+                # Only the main process executes this part, i.e. as soon as it receives the parallelly generated data
+                graph.edge_properties.loc[
+                    pd.MultiIndex.from_arrays(result_ids.T), "endfeet_id"
+                ] = result_endfeet
 
     return graph
 
