011aca230bef81c0b49eb61a45a7b74edecb8797
nico-canta <nicola.cantarutti@epfl.ch>
2023-10-12 17:37:43 +0200

    more asserts petsc2numpy

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -2,19 +2,19 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
+from mpi4py import MPI
+
 from astrovascpy.scipy_petsc_conversions import PETScVec2array
 from astrovascpy.scipy_petsc_conversions import array2PETScVec
 
 # pip install pytest-mpi
-# mpirun -n 2 pytest --with-mpi tests/test_mpi.py
+# mpirun -n 4 pytest --with-mpi tests/test_mpi.py
 
 
-# This test is skipped if not using the option --with-mpi 
+# This test is skipped if not using the option --with-mpi
 @pytest.mark.mpi(min_size=2)
 def test_petsc2numpy_array():
     """Test that the conversion from numpy array to petsc arrays works as expected"""
-
-    from mpi4py import MPI
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -29,10 +29,12 @@ def test_petsc2numpy_array():
     vec2_new = PETScVec2array(temp2)
     npt.assert_array_equal(vec2_new, vec2)
 
-    vec3 = np.arange(22) if rank == 0 else None
+    vec3 = np.arange(22, dtype=np.int64) if rank == 0 else None
     temp3 = array2PETScVec(vec3)
     vec3_new = PETScVec2array(temp3)
+    # array2PETScVec converts to float
     if rank == 0:
         assert vec3.dtype != vec3_new.dtype
-
-    npt.assert_array_equal(vec3_new, vec3)
+        assert vec3.dtype == np.int64
+        assert vec3_new.dtype == np.float64
+        npt.assert_array_equal(vec3_new, vec3.astype(np.float64))
