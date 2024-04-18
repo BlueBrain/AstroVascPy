25bce26c6ded9c782fcf622354398b5c39d14875
Nicola <nicola.cantarutti@epfl.ch>
2024-04-18 16:53:30 +0200

    Remove the copyright from readthedocs

--- astrovascpy/report_reader.py
+++ astrovascpy/report_reader.py
@@ -1,19 +1,21 @@
-"""Module dedicated to the report reading.
+# Copyright (c) 2023-2024 Blue Brain Project/EPFL
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module dedicated to the report reading.
 This is the standalone version before having proper sonata simulation config etc. The final version
 will use snap and will be in archngv most probably.
 Also this is a bit over-engineered but makes things easier if we have different types of reports
 in the future. Plus, this is a light version of the snap classes which will make things easy to
 adapt from these classes to the snap ones.
-Copyright (c) 2023-2024 Blue Brain Project/EPFL
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
 
 import numpy as np
